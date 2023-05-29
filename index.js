9c4e4cfcc916603c1bd7de4e8826d260cb4810b3
Tyler Swindell <tyler.swindell.design@gmail.com>
2023-05-29 13:02:43 -0400

    Added mobile nav animation

--- index.js
+++ index.js
@@ -3,39 +3,30 @@
 const navButton = document.querySelector(".hamburger-icon");
 const exitButton = document.querySelector(".exit-icon");
 const modalOverlay = document.getElementById("modal-overlay");
-const expandedNav = document.getElementById("expanded-nav");
-const expandedNavLinks = document.querySelectorAll("#expanded-nav>ul>li");
+const mobileNav = document.getElementById("mobile-nav");
+const mobileNavLinks = document.querySelectorAll("#mobile-nav>ul>li");
+
+console.log("Script Loaded");
 
 const toggleOverlay = () => {
-  if (modalOverlay.classList.contains("hidden")) {
+  modalOverlay.classList.toggle("show");
+  modalOverlay.classList.toggle("visible");
+  modalOverlay.classList.toggle("hidden");
+
+  if (modalOverlay.classList.contains("show")) {
     modalOverlay.style.marginTop = `${window.scrollY}px`;
-    modalOverlay.classList.add("visible");
-    modalOverlay.classList.remove("hidden");
     disableScroll();
-  } else {
-    modalOverlay.classList.add("hidden");
-    modalOverlay.classList.remove("visible");
-    enableScroll();
-  }
+  } else enableScroll();
 };
 
 const toggleNav = () => {
   toggleOverlay();
-  if (expandedNav.classList.contains("hidden")) {
-    expandedNav.style.marginTop = `${window.scrollY}px`;
-    expandedNav.classList.add("visible");
-    expandedNav.classList.remove("hidden");
-  } else {
-    expandedNav.classList.add("hidden");
-    expandedNav.classList.remove("visible");
-  }
+  mobileNav.classList.toggle("closed-nav");
 };
 
 navButton.addEventListener("click", toggleNav);
 exitButton.addEventListener("click", toggleNav);
-expandedNavLinks.forEach((item) => {
-  item.addEventListener("click", toggleNav);
-});
+mobileNavLinks.forEach((item) => item.addEventListener("click", toggleNav));
 
 document.addEventListener("keydown", (e) => {
   switch (e.key) {
